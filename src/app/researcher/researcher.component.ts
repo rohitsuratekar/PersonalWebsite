01d6783e005344a5bc813399b6bd7d6f02f3b5cb
Rohit Suratekar <rohitsuratekar@gmail.com>
2018-06-25 19:27:33 +0530

    Changes in meta-data

--- src/app/researcher/researcher.component.ts
+++ src/app/researcher/researcher.component.ts
@@ -50,23 +50,24 @@ class Infocard {
 }
 
 const ProjectDetails = [
-  {
-    'projectID': 0,
-    'title': 'Regulation of lipid signaling pathway in Drosophila melanogaster',
-    'duration': '2014 to 2018',
-    'keywords': ['Lipid Signaling', 'Steady State'],
-    'details': 'PLC mediated PIP2 hydrolysis is everywhere. Even after lot of research, how PIP2 signalling'
-    + ' is regulated is still not clear. To understand regulation in this signaling pathway, we built mathematical '
-    + 'model of this system.'
-  },
+
   {
     'projectID': 1,
     'title': 'Searching potential feedback links in existing signaling pathway',
     'duration': '2014 to present',
     'keywords': ['Feedback', 'Dynamics', 'Lipid Signaling'],
     'details': 'Drosophila phototransduction is one of the fastest G protein coupled reaction known '
-    + 'to humans. However kinetic properties of enzymes involved in this pathway are not able to explain '
-    + 'such a fast kinetics.'
+      + 'to humans. However kinetic properties of enzymes involved in this pathway are not able to explain '
+      + 'such a fast kinetics.'
+  },
+  {
+    'projectID': 0,
+    'title': 'Regulation of lipid signaling pathway in Drosophila melanogaster',
+    'duration': '2014 to 2018',
+    'keywords': ['Lipid Signaling', 'Steady State'],
+    'details': 'PLC mediated PIP2 hydrolysis is everywhere. Even after lot of research, how PIP2 signalling'
+      + ' is regulated is still not clear. To understand regulation in this signaling pathway, we built mathematical '
+      + 'model of this system.'
   },
   {
     'projectID': 2,
@@ -74,7 +75,7 @@ const ProjectDetails = [
     'duration': '2015 to present',
     'keywords': ['Lipid Signaling', 'Analytical'],
     'details': 'Lipids are polar molecules and can not diffuse freely through cytoplasm. In PIP2 cycle it '
-    + 'becomes problem because this signaling cascade is spanned over two different compartments. '
+      + 'becomes problem because this signaling cascade is spanned over two different compartments. '
   },
 
   {
@@ -83,7 +84,7 @@ const ProjectDetails = [
     'duration': '2013',
     'keywords': ['Lipid Signaling', 'Dynamics'],
     'details': 'Phosphatidic acid is important phospholipid which involved in lot of signaling and structural '
-    + 'functions inside cell. We try to understand how its levels are regulated through mathematical models.'
+      + 'functions inside cell. We try to understand how its levels are regulated through mathematical models.'
   },
   {
     'projectID': 4,
@@ -91,7 +92,7 @@ const ProjectDetails = [
     'duration': '2012',
     'keywords': ['Genemics', 'Codons', 'Bacteria'],
     'details': 'Codon bias has always fascinated researchers. We explored is there any connection between '
-    + 'protein content, codon bias and GC content inside various bacterial species. '
+      + 'protein content, codon bias and GC content inside various bacterial species. '
   },
   {
     'projectID': 5,
@@ -99,7 +100,7 @@ const ProjectDetails = [
     'duration': '2010 to 2011',
     'keywords': ['Microbiology', 'Growth', 'Bacteria'],
     'details': 'Bacteria has vital position in our tree of life. They are everywhere and affects '
-    + 'lot of our daily activities. We studied how turbulence might affect their growth.'
+      + 'lot of our daily activities. We studied how turbulence might affect their growth.'
   },
   {
     'projectID': 6,
@@ -107,7 +108,7 @@ const ProjectDetails = [
     'title': 'Spacial spread of virus infection on the bacterial lawn',
     'keywords': ['Virus', 'Growth', 'Bacteria'],
     'details': 'Mathematical model for analyzing spatial constrains on spread of viruses on bacterial plates.'
-    + 'Analysis was also extended to check multiple infections.'
+      + 'Analysis was also extended to check multiple infections.'
   },
   {
     'projectID': 7,
@@ -115,8 +116,8 @@ const ProjectDetails = [
     'duration': '2015',
     'keywords': ['Enzymes', 'Theory'],
     'details': 'Enzymes play key role in almost every cellular signaling. Their localization as well as spatial'
-    + 'distribution is very important for their function. We are trying to explore what are their advantages over'
-    + ' well mixed system with help of experimental as well as theoretical approaches.'
+      + 'distribution is very important for their function. We are trying to explore what are their advantages over'
+      + ' well mixed system with help of experimental as well as theoretical approaches.'
   }
 
 ];
