a041064bff811917b8b8c1ebb88195f902f9920a
Rohit Suratekar <rohitsuratekar@gmail.com>
2023-07-31 11:50:36 +0530

    Changed home page

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -4,14 +4,15 @@ const MainFooter = () => {
   return (
     <>
       <footer className="bg-background-100 py-4 text-center text-xs text-background-900">
-        <p>
-          © {new Date().getFullYear()} Rohit Suratekar.
+        <span>© {new Date().getFullYear()} Rohit Suratekar.</span>
+        <span className="visible sm:hidden"><br/></span>
+        <span>
           {" Please refer to the "}
           <Link to={"/credits"} className="text-link">
             Credits
           </Link>
           {" for license information."}
-        </p>
+        </span>
       </footer>
     </>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,35 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+const HomeCard = () => {
+  return (
+    <div className=" flex flex-col items-center md:flex-row md:space-x-4 bg-background-50 md:bg-white">
+      <div className="relative w-24 h-24 md:w-32 md:h-32 overflow-hidden flex-shrink-0 rounded-full mt-3 md:mt-0">
+        <img
+          className="object-cover w-full h-full absolute top-0 left-1/2 transform -translate-x-1/2 "
+          src="./rohit.jpg"
+          alt="Profile"
+        />
+      </div>
+      <div className="md:flex-grow">
+        <div className="flex bg-background-50">
+          <div className=" bg-primary w-3 hidden md:block"></div>
+          <div className="ml-3 py-3 pr-3">
+            <h2 className="text-xl font-semibold text-center md:text-left">
+              Rohit Suratekar
+            </h2>
+            <p className="mt-2 text-gray-600 italic text-center md:text-left">
+              My website is a vibrant canvas of growth, learning, and
+              creativity. It&rsquo;s a joyful journey where I embrace evolution,
+              share my passions, and celebrate progress 🚀
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const HomePage = () => {
   const homeLinks = useSelector((state) => state.app.homeLinks);
   return (
@@ -8,18 +37,8 @@ const HomePage = () => {
       <div className="h-screen grid grid-cols-1 sm:grid-cols-12">
         <div className="hidden sm:block  col-span-2 xl:col-span-3 bg-background-100" />
         <div className="col-span-8 xl:col-span-6 flex flex-col justify-center p-4">
-          <div className="mt-5 text-xs text-background">Why this website?</div>
-          <div className="flex bg-background-50 mt-5">
-            <div className=" bg-primary w-3"></div>
-            <div className="ml-3 py-3 pr-3">
-              <span className="italic">
-                Embracing growth and evolution, my personal website is the
-                canvas of my journey, celebrating the power of continuous
-                learning, progress, and unwavering accountability to showcase
-                the very best version of myself and my work.
-              </span>
-            </div>
-          </div>
+          <HomeCard />
+
           <div className="mt-5 text-xs text-background">
             Explore my work in following sections
           </div>
