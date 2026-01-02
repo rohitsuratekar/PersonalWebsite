c93b716959e06623b95c46e213648beca6629d2d
Rohit Suratekar <rohitsuratekar@gmail.com>
2026-01-02 11:20:49 +0530

    Updated conferences and publications

--- src/components/ImageLoader.jsx
+++ src/components/ImageLoader.jsx
@@ -3,6 +3,7 @@ import FlagDNK from "@/assets/img/flag-denmark.png"
 import FlagJPN from "@/assets/img/flag-japan.png"
 import FlagPOL from "@/assets/img/flag-poland.png"
 import FlagGER from "@/assets/img/flag-germany.png"
+import FlagUSA from "@/assets/img/flag-usa.png"
 
 // Flags citation: Icon made by Pixel perfect from www.flaticon.com
 
@@ -18,6 +19,8 @@ const getImageURL = (img) => {
             return FlagPOL;
         case "flag-germany":
             return FlagGER;
+        case "flag-usa":
+            return FlagUSA;
         default:
             return null
     }
